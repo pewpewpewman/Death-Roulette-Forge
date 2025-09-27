8ca19c68154ba80ad346f1a84f73fcc9d459e740
pewpewman <millerp2006@gmail.com>
2025-09-27 10:08:33 -0400

    added readme

--- src/main/java/com/github/pewpewpewman/death_roulette/DeathRoulette.java
+++ src/main/java/com/github/pewpewpewman/death_roulette/DeathRoulette.java
@@ -1,32 +1,36 @@
 package com.github.pewpewpewman.death_roulette;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.ChatFormatting;
+import net.minecraft.commands.arguments.ComponentArgument;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.chat.*;
+import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
+import net.minecraft.network.protocol.game.ClientboundSetTitlesAnimationPacket;
+import net.minecraft.network.protocol.game.ClientboundSoundPacket;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.LogicalSidedProvider;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraftforge.registries.*;
 import org.slf4j.Logger;
 
-import java.util.Optional;
-import java.util.function.Supplier;
+import java.util.List;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(Death_roulette.MODID)
-public class Death_roulette {
+@Mod(DeathRoulette.MODID)
+public class DeathRoulette {
 
     // Define mod id in a common place for everything to reference
     public static final String MODID = "death_roulette";
@@ -34,9 +38,15 @@ public class Death_roulette {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    private static final double ITEMLOSSCHANCE = 0.5;
+    private static double RESET_CHANCE = 0.0;
+    private static final DeferredRegister<SoundEvent> SOUND_EVENTS =
+            DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MODID);
 
-    public Death_roulette() {
+    private static final RegistryObject<SoundEvent> INVENTORY_CLEARED_SOUND = registerSoundEvents("inventory_cleared");
+    private static final RegistryObject<SoundEvent> INVENTORY_SPARE_SOUND = registerSoundEvents("inventory_spared");
+
+
+    public DeathRoulette() {
         System.out.println("DEATH ROULETTE INIT");
 
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -48,14 +58,29 @@ public class Death_roulette {
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(ServerModEvents.class);
 
+        //Register sounds
+        registerSounds(modEventBus);
+
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC, "death_roulette-common.toml");
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
+        //LOGGER.info("HELLO FROM COMMON SETUP");
+
+        RESET_CHANCE = Config.RESET_CHANCE.get();
+        //System.out.println("RESET CHANCE: " + RESET_CHANCE);
     }
+
+    public static void registerSounds(IEventBus modEventBus) {
+        SOUND_EVENTS.register(modEventBus);
+    }
+
+    private static RegistryObject<SoundEvent> registerSoundEvents(String name) {
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(new ResourceLocation(MODID, name)));
+    }
+
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.DEDICATED_SERVER)
@@ -63,129 +88,63 @@ public class Death_roulette {
 
         @SubscribeEvent
         public static void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
-            Player player = event.getEntity();
+            Player player_ = event.getEntity();
+            if (!(player_ instanceof ServerPlayer player)) return;
 
-            System.out.println("Player " + player.getName().getString() + " respawned!");
+            //Variables for sound and who hears the resulting sound
+            RegistryObject<SoundEvent> soundToPlay;
+            List<ServerPlayer> hearers;
+
+            //System.out.println("Player " + player.getName().getString() + " respawned!");
 
             double rng = Math.random();
-            if (rng > ITEMLOSSCHANCE) {
+            //System.out.println("RNG " + rng);
+
+            if (rng < RESET_CHANCE) {
+
+                //Clear out inventory
                 player.getInventory().clearContent();
-                System.out.println("Player " + player.getName().getString() + " loss their items!");
+                //System.out.println("Player " + player.getName().getString() + " loss their items!");
+
+                //Make whole server hear item loss noise
+                soundToPlay = INVENTORY_CLEARED_SOUND;
+                hearers = player.getServer().getPlayerList().getPlayers();
+
+                //Make clear text appear
+                player.connection.send(new ClientboundSetActionBarTextPacket(Component.translatable("death_roulette.inventory_cleared_action_bar").withStyle(ChatFormatting.RED).withStyle(ChatFormatting.BOLD)));
+                //player.connection.send(new ClientboundSetTitlesAnimationPacket(0, 100, 4));
+
+
+                //Tell entire server about this
+                for(ServerPlayer serverPlayer : player.getServer().getPlayerList().getPlayers()) {
+                    serverPlayer.sendSystemMessage(
+                            Component.translatable("death_roulette.inventory_cleared_server_message", player.getDisplayName()).withStyle(ChatFormatting.RED),
+                            false);
+                }
             }
+            else {
+                //Play sound only for player who died
+                soundToPlay = INVENTORY_SPARE_SOUND;
+                hearers = List.of(player);
+            }
+
+            //Play Sound(s)
+            for (ServerPlayer iterPlayer : hearers)
+            {
+                iterPlayer.connection.send(new ClientboundSoundPacket(
+                        soundToPlay.getHolder().get(),
+                        SoundSource.AMBIENT,
+                        iterPlayer.position().x(),
+                        iterPlayer.position().y(),
+                        iterPlayer.position().z(),
+                        1.0f,
+                        1.0f,
+                        0
+                ));
+            }
+
         }
     }
 
-    //Packet Handler for Death Roulette - needed for activating logical client side effects
-    //on inventory clear
-    public class StartupCommon
-    {
-        public static SimpleChannel simpleChannel;
 
-        public static final byte INVENTORY_CLEARED_PROC_ID = 49;
-        public static final byte INVENTORY_SPARED_PROC_ID = 83;
-
-        public static final String MESSAGE_PROTOCOL_VERSION = "1.0";
-
-        public static final ResourceLocation simpleChannelRl = new ResourceLocation(MODID, "drchannel");
-
-        public static void onCommonSetup(FMLCommonSetupEvent event) {
-            simpleChannel = NetworkRegistry.newSimpleChannel(
-                    simpleChannelRl,
-                    () -> MESSAGE_PROTOCOL_VERSION,
-                    MESSAGE_PROTOCOL_VERSION::equals,
-                    MESSAGE_PROTOCOL_VERSION::equals
-            );
-
-            simpleChannel.registerMessage(
-                    INVENTORY_CLEARED_PROC_ID,
-                    InventoryClearedMessageToClient.class,
-                    InventoryClearedMessageToClient::encode,
-                    InventoryClearedMessageToClient::decode,
-
-            );
-
-            simpleChannel.registerMessage(
-                    INVENTORY_SPARED_PROC_ID,
-                    InventorySparedMessageToClient.class,
-                    InventorySparedMessageToClient::encode,
-                    InventorySparedMessageToClient::decode
-                    );
-
-        }
-
-        public static void onServerReceivedClearedMessage(
-                final InventoryClearedMessageToClient message,
-                Supplier<NetworkEvent.Context> ctxSupplier
-        ) {
-            NetworkEvent.Context ctx = ctxSupplier.get();
-            LogicalSide sideReceived = ctx.getDirection().getReceptionSide();
-            ctx.setPacketHandled(true);
-
-            if (sideReceived != LogicalSide.CLIENT) {
-                LOGGER.warn("TargetEffectMessageToClient received on wrong side:" + ctx.getDirection().getReceptionSide());
-                return;
-            }
-            if (!message.isMessageIsValid()) {
-                LOGGER.warn("TargetEffectMessageToClient was invalid" + message.toString());
-                return;
-            }
-            // we know for sure that this handler is only used on the client side, so it is ok to assume
-            //  that the ctx handler is a client, and that Minecraft exists.
-            // Packets received on the server side must be handled differently!  See MessageHandlerOnServer
-
-            Optional<ClientWorld> clientWorld = LogicalSidedProvider.CLIENTWORLD.get(sideReceived);
-            if (!clientWorld.isPresent()) {
-                LOGGER.warn("TargetEffectMessageToClient context could not provide a ClientWorld.");
-                return;
-            }
-        }
-    }
-
-    public static class InventoryClearedMessageToClient {
-
-        private boolean messageIsValid;
-
-        private InventoryClearedMessageToClient()
-        {
-            messageIsValid = false;
-        }
-
-        public boolean isMessageIsValid() {
-            return messageIsValid;
-        }
-
-        public static InventoryClearedMessageToClient decode(FriendlyByteBuf buf) {
-            InventoryClearedMessageToClient ret = new InventoryClearedMessageToClient();
-            ret.messageIsValid = true;
-            return ret;
-        }
-
-        public void encode(FriendlyByteBuf buf) {
-            if (!messageIsValid) {
-                return;
-            }
-        }
-    }
-
-    public static class InventorySparedMessageToClient {
-
-        private boolean messageIsValid;
-
-        private InventorySparedMessageToClient()
-        {
-            messageIsValid = false;
-        }
-
-        public boolean isMessageIsValid() {
-            return messageIsValid;
-        }
-
-        public static InventorySparedMessageToClient decode(FriendlyByteBuf buf) {
-            InventorySparedMessageToClient ret = new InventorySparedMessageToClient();
-            ret.messageIsValid = true;
-            return ret;
-        }
-        public void encode(FriendlyByteBuf buf) {}
-
-    }
 }
